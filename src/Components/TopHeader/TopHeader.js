03ddfdaa47bed73b34e3af862503135c814408ad
Shipon Hossen Raju <msshipon234@gmail.com>
2024-04-30 22:19:45 +0600

    project setup and design complete

---
+++ src/Components/TopHeader/TopHeader.js
@@ -0,0 +1,75 @@
+import { SearchOutlined, BellOutlined, PlusOutlined } from "@ant-design/icons";
+import { Input, theme } from "antd";
+import Image from "next/image";
+const { useToken } = theme;
+
+import maleAvatar from "@/Assets/avatar/male.png";
+import femaleAvatar from "@/Assets/avatar/female.jpg";
+
+// Desc: TopHeader component for the website
+export default function TopHeader() {
+  const gender = "female";
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = useToken();
+
+  // onSearch function
+  const onSearch = (value) => {
+    console.log(value);
+  };
+
+  return (
+    <header
+      style={{
+        padding: 24,
+        margin: "24px 16px 0",
+        background: colorBgContainer,
+        borderRadius: borderRadiusLG,
+      }}
+    >
+      <div className="flex items-center justify-between">
+        <div>
+          <Input
+            size="large"
+            placeholder="Search Now..."
+            prefix={<SearchOutlined />}
+            onSearch={onSearch}
+          />
+        </div>
+        <div className="flex items-center justify-end gap-4">
+          {/* add project */}
+          <button className="focus:outline-none w-12 h-12 rounded-full p-1.5 border text-lg">
+            <PlusOutlined />
+          </button>
+
+          {/* Notification */}
+          <button className="focus:outline-none w-12 h-12 rounded-full p-1.5 border text-lg">
+            <BellOutlined />
+          </button>
+
+          {/* user card */}
+          <div className="flex items-center justify-end gap-3">
+            <figure className="w-12 h-12 rounded-full p-1.5 border">
+              {gender == "female" ? (
+                <Image
+                  className="w-full h-full"
+                  src={femaleAvatar}
+                  alt="user"
+                />
+              ) : (
+                <Image className="w-full h-full" src={maleAvatar} alt="user" />
+              )}
+            </figure>
+            <div>
+              <h2 className="font-semibold text-lg">Shipon Hossen</h2>
+              <p className="font-normal text-base text-slate-500">
+                Project Manager
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,136 @@
+"use client";
+
+// antd design imports
+import {
+  AppstoreOutlined,
+  CalendarOutlined,
+  LineChartOutlined,
+  MessageOutlined,
+  ProjectOutlined,
+  SafetyOutlined,
+  ScheduleOutlined,
+  SettingOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+const { useToken } = theme;
+const { Header, Content, Footer, Sider } = Layout;
+
+import logo from "@/Assets/logo.png";
 import Image from "next/image";
+import TopHeader from "@/Components/TopHeader/TopHeader";
+
+// nav items
+const items = [
+  {
+    key: "side-nav-dashboard",
+    icon: <AppstoreOutlined />,
+    label: "Dashboard",
+  },
+  {
+    key: "side-nav-MyTasks",
+    icon: <ScheduleOutlined />,
+    label: "My Tasks",
+  },
+  {
+    key: "side-nav-projects",
+    icon: <ProjectOutlined />,
+    label: "Projects",
+  },
+  {
+    key: "side-nav-Calendar",
+    icon: <CalendarOutlined />,
+    label: "Calendar",
+  },
+  {
+    key: "side-nav-Feed",
+    icon: <SafetyOutlined />,
+    label: "Feed",
+  },
+  {
+    key: "side-nav-Messages",
+    icon: <MessageOutlined />,
+    label: "Messages",
+  },
+  {
+    key: "side-nav-Goals",
+    icon: <LineChartOutlined />,
+    label: "Goals",
+  },
+  {
+    key: "side-nav-Team",
+    icon: <TeamOutlined />,
+    label: "Team",
+  },
+  {
+    key: "side-nav-Settings",
+    icon: <SettingOutlined />,
+    label: "Settings",
+  },
+];
 
 export default function Home() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = useToken();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Layout className="min-h-screen">
+      {/* side bar */}
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+        theme="light"
+      >
+        <div className="demo-logo-vertical max-w-24 rounded-2xl shadow-lg mx-auto mt-8 mb-4">
+          <Image className="w-full h-full rounded-2xl" src={logo} alt="logo" />
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        <Menu
+          theme="light"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
         />
-      </div>
+      </Sider>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* content side */}
+      <Layout>
+        <TopHeader />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Main Content */}
+        <Content
+          style={{
+            margin: "24px 16px 0",
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            content
+          </div>
+        </Content>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Footer */}
+        <Footer
+          style={{
+            textAlign: "center",
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
